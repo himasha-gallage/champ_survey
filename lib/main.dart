2f058630702c3205fde90734aa1b112a32cf25a0
himasha-gallage <himasha@champitsolutions.com>
2020-09-25 11:10:45 +0530

    add repository

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/login_section/welcomePage.dart';
 import 'bloc/auth_bloc/barrell.dart';
 import 'bloc/survey_bloc/barrell.dart';
 import 'bloc/simple_bloc_delegate.dart';
+import 'entities/barrell.dart';
 
 void main() {
   BlocSupervisor.delegate = SimpleBlocDelegate();
@@ -20,14 +22,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         textTheme: GoogleFonts.latoTextTheme(textTheme).copyWith(
           bodyText1: GoogleFonts.montserrat(textStyle: textTheme.bodyText1),
         ),
       ),
-      debugShowCheckedModeBanner: false,
-      home: WelcomePage(),
+      // home: WelcomePage(),
+      home: MultiProvider(
+        providers: [
+          Provider<User>(
+            create: (BuildContext context) => User(),
+          ),
+        ],
+        child: MultiRepositoryProvider(
+          providers: [],
+          child: MultiBlocProvider(providers: null, child: null),
+        ),
+      ),
     );
   }
 }
---
+++ lib/repository/auth_repository.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import '../entities/barrell.dart';
+
+class AuthRepository {
+  Future<User> signInWithEmailAndPassword({
+    @required String email,
+    @required String password,
+  }) {}
+}
